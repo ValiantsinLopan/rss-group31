3be37eefbe010f24a9b4faf275237c42d003110b
Valentin Lopan <valiantsin_lopan@live.com>
2019-01-26 15:19:47 +0300

    feat: add initial project

---
+++ architects/src/screens/architects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+class Architects extends Component {
+  render() {
+    return (
+      <Paper>
+        <Typography>
+            Belarusian architects
+        </Typography>
+      </Paper>
+    );
+  }
+}
+
+export default Architects;
