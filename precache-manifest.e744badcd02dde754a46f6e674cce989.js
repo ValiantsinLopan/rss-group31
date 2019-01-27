b95d2fde2fd8ace05533ed5985b246dfbb301059
Valentin Lopan <valiantsin_lopan@live.com>
2019-01-27 17:04:17 +0300

    Updates

---
+++ precache-manifest.e744badcd02dde754a46f6e674cce989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2df9f06a1b57662438b7",
+    "url": "/rss-group31/static/js/runtime~main.2df9f06a.js"
+  },
+  {
+    "revision": "4f6013eb3e3b2d1270e7",
+    "url": "/rss-group31/static/js/main.4f6013eb.chunk.js"
+  },
+  {
+    "revision": "29bc3c1a2195d105e61e",
+    "url": "/rss-group31/static/js/1.29bc3c1a.chunk.js"
+  },
+  {
+    "revision": "4f6013eb3e3b2d1270e7",
+    "url": "/rss-group31/static/css/main.9e824b8f.chunk.css"
+  },
+  {
+    "revision": "15a4d2c0f7557998dd80a7868300114c",
+    "url": "/rss-group31/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rss-group31/precache-manifest.0ca33b7d79eae7163cbb72c1decda938.js"
+  "/rss-group31/precache-manifest.e744badcd02dde754a46f6e674cce989.js"
 );
 
 workbox.clientsClaim();
