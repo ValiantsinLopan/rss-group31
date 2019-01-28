75c5a617b25e398aad6eadb4966a614a7b30342f
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2019-01-28 20:17:14 +0300

    Updates

---
+++ precache-manifest.0617fc4927a138f839cae3c13c7f6efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2df9f06a1b57662438b7",
+    "url": "/rss-group31/static/js/runtime~main.2df9f06a.js"
+  },
+  {
+    "revision": "524536e8416ab133b1ff",
+    "url": "/rss-group31/static/js/main.524536e8.chunk.js"
+  },
+  {
+    "revision": "1da465187329af0a4047",
+    "url": "/rss-group31/static/js/1.1da46518.chunk.js"
+  },
+  {
+    "revision": "524536e8416ab133b1ff",
+    "url": "/rss-group31/static/css/main.3e7ffc6d.chunk.css"
+  },
+  {
+    "revision": "11a481e60af1368ad1b87b52194da359",
+    "url": "/rss-group31/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rss-group31/precache-manifest.80c910e40f3494f7d458cc5b368d02cd.js"
+  "/rss-group31/precache-manifest.0617fc4927a138f839cae3c13c7f6efc.js"
 );
 
 workbox.clientsClaim();
